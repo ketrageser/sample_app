ebd22f052478723090bf1f8fc0592294dab33686
Kelly Trageser <kelty@cablespeed.com>
2012-01-18 09:40:07 -0500

    Added a Site Layout

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -9,10 +9,15 @@ describe PagesController do
       response.should be_success
     end
     
-    it  "should have the right title" do
+    it "should have the right title" do
       get 'home'
       response.should have_selector("title", 
         :content => "Ruby on Rails Tutorial Sample App | Home")
+      end
+      
+    it "should have a nonblank body" do
+      get 'home'
+      response.body.should_not =~ /<body>\s*<\/body>/
     end
   end
 
@@ -21,12 +26,34 @@ describe PagesController do
       get 'contact'
       response.should be_success
     end
+    
+    it "should have the right title" do
+      get 'contact'
+      response.should have_selector("title", 
+        :content => "Ruby on Rails Tutorial Sample App | Contact")
+      end
+      
+    it "should have a nonblank body" do
+      get 'contact'
+      response.body.should_not =~ /<body>\s*<\/body>/
+    end
   end
   
     describe "GET 'about'" do
     it "returns http success" do
       get 'about'
       response.should be_success
+    end
+    
+    it "should have the right title" do
+      get 'about'
+      response.should have_selector("title", 
+        :content => "Ruby on Rails Tutorial Sample App | About")
+      end
+      
+    it "should have a nonblank body" do
+      get 'about'
+      response.body.should_not =~ /<body>\s*<\/body>/
     end
   end
 
