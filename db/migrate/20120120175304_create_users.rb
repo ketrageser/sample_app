1a5bb373debacb52a028f5a762928b146217bfb4
Kelly Trageser <kelty@cablespeed.com>
2012-01-20 14:31:22 -0500

    user model validation

--- db/migrate/20120120175304_create_users.rb
+++ db/migrate/20120120175304_create_users.rb
@@ -1,5 +1,6 @@
 class CreateUsers < ActiveRecord::Migration
   def change
+    drop_table :users
     create_table :users do |t|
       t.string :name
       t.string :email
@@ -7,4 +8,5 @@ class CreateUsers < ActiveRecord::Migration
       t.timestamps
     end
   end
+
 end
