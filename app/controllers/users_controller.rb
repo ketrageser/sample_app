c218fa1cfd29f564cd579ffd12b2138c7600e84f
Kelly Trageser <kelty@cablespeed.com>
2012-01-31 14:37:35 -0500

    Completed signin/out

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,8 +13,9 @@ class UsersController < ApplicationController
   def create
     @user = User.new(params[:user])
     if @user.save
+      sign_in @user
       flash[:success] = "Welcome to the Sample App!"
-      redirect_to @user, :flash => {:success => "Welcome to the Sample App!" }
+      redirect_to @user, :flash => { :success => "Welcome to the Sample App!" }
     else
       @title = "Sign up"
       render 'new'
