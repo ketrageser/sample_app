85a3c871513615e5306c0952d8d13250124ab10c
Kelly Trageser <kelty@cablespeed.com>
2012-01-31 11:34:07 -0500

    Working user signin

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,6 +1,6 @@
 Factory.define :user do |user|
-  user.name                         "Kelly Trageser"
-  user.email                         "ktrageser@example.com"
+  user.name                         "Michael Hartl"
+  user.email                         "mhartl@example.com"
   user.password                   "foobar"
   user.password_confirmation "foobar"
 end
